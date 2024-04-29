b3089a502fd7f705fdea2861ea13b5be4ca2853c
richardl2003 <richardliy03@gmail.com>
2024-04-29 14:43:51 -0400

    feat: added state and made homepage dynamic to filters

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,52 @@
-import { listings } from '@/api/data/listings';
+import { useState } from 'react';
+
+import {
+  isListingAvailable,
+  listings as staticListings,
+} from '@/api/data/listings';
+import ListingFilters from '@/components/ListingFilters';
 import ListingList from '@/components/ListingList';
+import { Separator } from '@/components/ui';
 
 const HomePage = () => {
+  const [listings, setListings] = useState(staticListings);
+
+  const handleFilters = (filters) => {
+    const { dates, guests, search } = filters;
+
+    // Resets filters by using static listings
+    let filteredListings = staticListings;
+
+    // Handles date range
+    if (dates) {
+      filteredListings = filteredListings.filter((listing) =>
+        isListingAvailable(listing, dates),
+      );
+    }
+
+    // Handles guests
+    if (guests) {
+      filteredListings = filteredListings.filter(
+        (listing) => guests <= listing.maxGuests,
+      );
+    }
+
+    // Handles search
+    if (search) {
+      filteredListings = filteredListings.filter((listing) =>
+        listing.name.toLowerCase().includes(search.toLowerCase()),
+      );
+    }
+
+    setListings(filteredListings);
+  };
+
   return (
     <div className='container py-4'>
+      <div className='mb-4'>
+        <ListingFilters onChange={handleFilters} />
+        <Separator className='my-4' />
+      </div>
       <ListingList listings={listings} />
     </div>
   );
