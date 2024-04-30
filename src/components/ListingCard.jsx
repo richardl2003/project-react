3d7993acd90c442d258a6562cf64f96b619e0f07
richardl2003 <richardliy03@gmail.com>
2024-04-30 18:56:00 -0400

    feat: added effects to get listings from external API

--- src/components/ListingCard.jsx
+++ src/components/ListingCard.jsx
@@ -1,3 +1,4 @@
+import { DollarSign, Pin, Users } from 'lucide-react';
 import { Card, CardContent } from '@/components/ui';
 import { getImageUrl } from '@/lib/utils/images';
 
@@ -9,8 +10,25 @@ const ListingCard = ({ listing }) => {
         src={getImageUrl(listing.images[0])}
         alt={listing.name}
       />
-      <CardContent className='p-4'>
-        <h2 className='mb-0 text-xl font-semibold'>{listing.name}</h2>
+      <CardContent className='flex flex-col gap-2 p-4'>
+        <h2 className='mb-2 text-xl font-semibold'>{listing.name}</h2>
+        <div className='flex items-center gap-2'>
+          <DollarSign className='h-4 w-4 text-primary' />
+          <span className='text-muted-foreground'>
+            <span className='font-bold text-foreground'>{listing.price}</span> /
+            night
+          </span>
+        </div>
+        <div className='flex items-center gap-2'>
+          <Pin className='h-4 w-4 text-primary' />
+          <span className='text-muted-foreground'>{listing.location.name}</span>
+        </div>
+        <div className='flex items-center gap-2'>
+          <Users className='h-4 w-4 text-primary' />
+          <span className='text-muted-foreground'>
+            {listing.maxGuests} Guests
+          </span>
+        </div>
       </CardContent>
     </Card>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,44 +1,72 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useState, useEffect, useRef } from 'react';
 
-import {
-  isListingAvailable,
-  listings as staticListings,
-} from '@/api/data/listings';
+import api from '@/api';
 import ListingFilters from '@/components/ListingFilters';
 import ListingList from '@/components/ListingList';
-import { Separator } from '@/components/ui';
+import { Separator, Spinner } from '@/components/ui';
 
 const HomePage = () => {
-  const [listings, setListings] = useState(staticListings);
+  const [listings, setListings] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [filters, setFilters] = useState({
+    dates: undefined,
+    guests: 0,
+    search: '',
+  });
+
+  // useRef is used to persist the value between renders
+  const abortController = useRef(null);
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      // Reset to default before new request
+      setIsLoading(true);
+      setError(null);
+
+      abortController.current = new AbortController();
+
+      try {
+        const response = await api.get('/api/listings', {
+          params: filters,
+          signal: abortController.current.signal,
+        });
+        setListings(response.data);
+      } catch (error) {
+        if (axios.isCancel(error)) {
+          return;
+        }
+        setError('Something went wrong. Please try again later.');
+      } finally {
+        setIsLoading(false);
+      }
+
+      return () => {
+        abortController.current.abort();
+      };
+    };
+
+    fetchListings();
+  }, [filters]);
 
   const handleFilters = (filters) => {
-    const { dates, guests, search } = filters;
+    setFilters(filters);
+  };
 
-    // Resets filters by using static listings
-    let filteredListings = staticListings;
-
-    // Handles date range
-    if (dates) {
-      filteredListings = filteredListings.filter((listing) =>
-        isListingAvailable(listing, dates),
+  const renderListingList = () => {
+    if (isLoading) {
+      return (
+        <div className='flex justify-center'>
+          <Spinner />
+        </div>
       );
     }
 
-    // Handles guests
-    if (guests) {
-      filteredListings = filteredListings.filter(
-        (listing) => guests <= listing.maxGuests,
-      );
+    if (error) {
+      return <div className='text-center'>{error}</div>;
     }
-
-    // Handles search
-    if (search) {
-      filteredListings = filteredListings.filter((listing) =>
-        listing.name.toLowerCase().includes(search.toLowerCase()),
-      );
-    }
-
-    setListings(filteredListings);
+    return <ListingList listings={listings} />;
   };
 
   return (
@@ -47,7 +75,7 @@ const HomePage = () => {
         <ListingFilters onChange={handleFilters} />
         <Separator className='my-4' />
       </div>
-      <ListingList listings={listings} />
+      {renderListingList()}
     </div>
   );
 };
